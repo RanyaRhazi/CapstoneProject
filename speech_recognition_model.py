f3b1ff621fe2edbeaa8bc23fe6345526b2c00181
RanyaRhazi <124635253+RanyaRhazi@users.noreply.github.com>
2023-11-06 18:18:38 +0100

    Update speech_recognition_model.py

--- speech_recognition_model.py
+++ speech_recognition_model.py
@@ -1,45 +1,37 @@
+import torchaudio
 import sounddevice as sd
 import numpy as np
-import speech_recognition as sr
-#import librosa
-import noisereduce as nr
-
+import torch
+from transformers import Wav2Vec2ForCTC, Wav2Vec2Tokenizer
 
 def transcribe_realtime_speech():
     # Audio sampling parameters
-    sample_rate = 44100  
+    sample_rate = 16000
     duration = 10
-    
+
+    # Load the Wav2Vec2 model and tokenizer
+    model_name = "facebook/wav2vec2-base-960h"
+    tokenizer = Wav2Vec2Tokenizer.from_pretrained(model_name)
+    model = Wav2Vec2ForCTC.from_pretrained(model_name)
+
     # Audio Recording
     print("Recording...")
     audio_data = sd.rec(int(sample_rate * duration), samplerate=sample_rate, channels=1)
     sd.wait()
 
     audio_data = audio_data[:, 0]
-    
-    noisy_part = audio_data
-    reduced_noise = nr.reduce_noise(y=noisy_part, sr=sample_rate)
-    
-    # Speech recognizer
-    recognizer = sr.Recognizer()
 
-    # Audio Data COnversion
-    audio_source = sr.AudioData(
-        reduced_noise.tobytes(),
-        sample_rate=sample_rate,
-        sample_width=reduced_noise.dtype.itemsize  # Sample Width
-    )
-    
+    # Convert the audio data to a format recognized by the model
+    audio_signal = torch.tensor(audio_data)
 
-    # Speech recognition
-    try:
-        transcribed_text = recognizer.recognize_google(audio_source, language="en-US")
-        print("Transcribed Speech:")
-        print(transcribed_text)
-    except sr.UnknownValueError:
-        print("Speech Recognition could not understand the audio")
-    except sr.RequestError as e:
-        print(f"Could not request results from Google Speech Recognition service; {e}")
+    # Perform real-time speech recognition
+    with torch.no_grad():
+        input_values = tokenizer(audio_data, return_tensors="pt").input_values
+        logits = model(input_values).logits
+        predicted_ids = torch.argmax(logits, dim=-1)
+        transcription = tokenizer.batch_decode(predicted_ids)
 
-if __name__ == "__main__":
-    transcribe_realtime_speech()
+    return transcription[0]
+
+
+
