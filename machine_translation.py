df70ffca2c0b6f4e701527048c90aef1cd1f7ca9
RanyaRhazi <124635253+RanyaRhazi@users.noreply.github.com>
2025-05-13 11:09:09 -0400

    Update machine_translation.py

--- machine_translation.py
+++ machine_translation.py
@@ -1,11 +1,12 @@
 from transformers import MarianMTModel, MarianTokenizer
 
+# Load the translation model and tokenizer 
+## for production load once globally and reuse instead of re-downloading every function call
+model_name = f'Helsinki-NLP/opus-mt-{source_language}-{target_language}'
+model = MarianMTModel.from_pretrained(model_name)
+tokenizer = MarianTokenizer.from_pretrained(model_name)
+
 def translate_text(text, source_language, target_language):
-    # Load the translation model and tokenizer 
-    ## for production load once globally and reuse instead of re-downloading every function call
-    model_name = f'Helsinki-NLP/opus-mt-{source_language}-{target_language}'
-    model = MarianMTModel.from_pretrained(model_name)
-    tokenizer = MarianTokenizer.from_pretrained(model_name)
 
     # Tokenize the input text and generate translation
     inputs = tokenizer(text, return_tensors="pt", padding=True, truncation=True)
